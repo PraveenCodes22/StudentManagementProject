274717f912f3898fa4d9d8c69521a3983f4ad4ff
PraveenCodes22 <nagapraveenkatta2000@gmail.com>
2024-08-20 12:12:05 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,29 @@
+// Student.java
+public class Student {
+    private String studentId;
+    private String name;
+    private int age;
+
+    public Student(String studentId, String name, int age) {
+        this.studentId = studentId;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student ID: " + studentId + ", Name: " + name + ", Age: " + age;
+    }
+}
